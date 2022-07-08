aa8a83bcc3d04ffd034edcef12bf7da91ef8d48a
Mariana Nunes <marinun47@gmail.com>
2022-07-08 19:26:08 -0300

    adiciona .env.exemple

--- atividade-semanal/atividade-sala/src/config/dbConnect.js
+++ atividade-semanal/atividade-sala/src/config/dbConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require ("mongoose");
 
-mongoose.connect("mongodb+srv://anairam_fn:mari2409@cluster0.mmjbu.mongodb.net/reprograma");
+mongoose.connect(`mongodb+srv://${process.env.ATLAS_LOGIN}:${process.env.ATLAS_PASSWORD}@cluster0.mmjbu.mongodb.net/atividade-semanal12`);
 
 let db = mongoose.connection;
 
