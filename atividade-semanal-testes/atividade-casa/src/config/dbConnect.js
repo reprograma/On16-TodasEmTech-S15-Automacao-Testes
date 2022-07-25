63b1e8bdaa282213bf3b71d063ad670b58a12d78
Letícia Rodrigues <leticiab.rodrigues@outlook.com>
2022-07-25 12:46:24 -0300

    adiciona atividade casa - testes :test_tube:

---
+++ atividade-semanal-testes/atividade-casa/src/config/dbConnect.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb+srv://letbatista2:hRpDSF65871@cluster0.qyzo3.mongodb.net/aula15testes')
+
+let db = mongoose.connection;
+
+module.exports = db;
